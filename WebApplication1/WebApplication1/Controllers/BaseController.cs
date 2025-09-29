328fd8d86c13116fed518a1cd11c590ed646550c
Antonin <antonin.miranda@epitech.eu>
2025-09-29 12:44:24 +0100

    [+] project base

---
+++ WebApplication1/WebApplication1/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("/")]
+public class BaseController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { message = "Hello World" });
+    } 
+}
---
+++ WebApplication1/WebApplication1/Controllers/Qcm.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Text.Json;
+
+namespace WebApplication1.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class QcmController : ControllerBase
+{
+    [HttpGet("{count}")]
+    public IActionResult Get(int count)
+    {
+        var jsonPath = "Data/Anatomie.json";
+        if (!System.IO.File.Exists(jsonPath))
+            return NotFound("Fichier JSON non trouvé.");
+
+        var json = System.IO.File.ReadAllText(jsonPath);
+        var questions = JsonSerializer.Deserialize<List<Qcm>>(json);
+
+        if (questions == null || questions.Count == 0)
+            return NotFound("Aucune question disponible.");
+
+        var random = new Random();
+        var selected = questions.OrderBy(x => random.Next()).Take(count).ToList();
+
+        return Ok(selected);
+    }
+}
