dd282c90aaf51a90186ae79b4fc76c60691315cd
Antonin <antonin.miranda@epitech.eu>
2025-10-01 15:50:37 +0100

    [+] docker config for run backend

---
+++ backend/backend/Models/Qcm.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models;
+
+public class Qcm
+{
+    public int Id { get; set; }
+    public string Question { get; set; }
+    public Dictionary<string, string> Choices { get; set; }
+    public string Answer { get; set; } 
+}
