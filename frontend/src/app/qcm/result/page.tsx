2583eb9546637e77386d7e15a1f2496b2ae74a42
Antonin <antonin.miranda@epitech.eu>
2025-10-04 17:48:12 +0100

    style(frontend): page result refont

--- frontend/src/app/qcm/result/page.tsx
+++ frontend/src/app/qcm/result/page.tsx
@@ -2,24 +2,37 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
+import { Card, CardHeader, CardTitle, CardContent, CardAction, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default function ResultPage() {
     console.log("coucu")
+    const router = useRouter();
     const searchParams = useSearchParams();
     const score = Number(searchParams.get("score") || 0);
     const total = Number(searchParams.get("total") || 0);
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1 className="text-2xl font-bold mb-4">Résultat du QCM</h1>
-            <p className="text-lg mb-2">
-                Vous avez obtenu {score} / {total} bonnes réponses.
-            </p>
-            {score === total ? (
-                <p className="text-green-600 font-semibold">Félicitations, score parfait !</p>
-            ) : (
-                <p className="text-blue-600">Essayez encore pour améliorer votre score.</p>
-            )}
+        <div className="flex items-center justify-center min-h-screen">
+            <Card className="w-[350px]">
+                <CardHeader>
+                    <CardTitle>Résultat du QCM</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <p className="text-lg mb-2">
+                        Vous avez obtenu {score} / {total} bonnes réponses.
+                    </p>
+                    {score === total ? (
+                        <p className="text-green-600 font-semibold">Félicitations, score parfait !</p>
+                    ) : (
+                        <p className="text-blue-600">Essayez encore pour améliorer votre score.</p>
+                    )}
+                </CardContent>
+                <CardFooter>
+                    <Button onClick={() => router.push("/")}>Accueil</Button>
+                    <Button onClick={() => router.push(`/qcm?nbQuestions=${total}`)}>Recommencer</Button>
+                </CardFooter>
+            </Card>
         </div>
     );
 }
